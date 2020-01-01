ff253806f02df2d5de66568a336ebd3df6758f6e
kdw9502 <kdw9502@gmail.com>
2020-01-01 19:14:47 +0900

    no message

--- src/main.rs
+++ src/main.rs
@@ -4,4 +4,13 @@ fn main() {
     mutable_text = text;
     println!("{0}", mutable_text);
     //https://doc.rust-lang.org/book/ch03-01-variables-and-mutability.html
+
+    let var: u64 = "452".parse().unwrap();
+    let func = func1;
+    print!("{}",func(1));
+}
+
+fn func1(a:i32) -> i32
+{
+    6
 }
